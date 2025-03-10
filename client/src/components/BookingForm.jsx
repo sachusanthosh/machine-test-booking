24f8857fbf5fabc67182b4b555529554bcf64d87
Sachu Santhosh <132931633+sachusanthosh@users.noreply.github.com>
2025-03-10 17:45:09 +0530

    Update BookingForm.jsx

--- client/src/components/BookingForm.jsx
+++ client/src/components/BookingForm.jsx
@@ -7,11 +7,11 @@ const BookingForm = ({ onBookingSuccess }) => {
   const [service, setService] = useState("Car Wash");
   const [date, setDate] = useState("");
 
-//   posting bookings to db
+  // posting bookings to db
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://machine-test-booking-server.onrender.com", {
+      const response = await axios.post("https://machine-test-booking-server.onrender.com/bookings", {
         name,
         email,
         service,
